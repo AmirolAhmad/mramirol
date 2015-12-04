197fad85c9ab5ed8d9c3f3fbcf831426a003ba9f
Amirol Ahmad <9w2pdf@gmail.com>
2015-12-04 15:19:57 +0800

    remove unuse lib

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,6 +1,4 @@
 class Admin::DashboardsController < AdminController
-  before_filter :store_location, only: [:index]
-
   def index
   end
 end
--- lib/authenticable.rb
+++ lib/authenticable.rb
@@ -11,26 +11,4 @@ module Authenticable
       redirect_to dashboard_path
     end
   end
-
-  def require_user
-    unless current_user
-      store_location
-      flash[:alert] = "You must be logged in to view this page."
-      redirect_to login_url
-      return false
-    end
-  end
-
-  def store_location
-    session[:return_to] = request.original_url rescue nil
-  end
-
-  def redirect_back_or_default(default)
-    redirect_to (session[:return_to] || default)
-    session[:return_to] = nil
-  end
-
-  def get_redirect_back_or_default(default)
-    redirect = session[:return_to] ? session[:return_to] : default
-  end
 end
